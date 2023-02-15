5ac1d87e32a004a2538a7e2a210a06587ad3c1b1
soonhyong <mikekwon42@gmail.com>
2023-02-15 21:47:46 +0900

    [추가] 기본 데이터에서 feature column 추출

--- Data Analysis3/module/outlier.py
+++ Data Analysis3/module/outlier.py
@@ -1,3 +1,6 @@
+import pandas as pd 
+import numpy as np
+
 def remove(data,column):
     quant1 =data[column].quantile(0.25)
     quant2 =data[column].quantile(0.5)
@@ -11,3 +14,26 @@ def remove(data,column):
     data=data[(data[column] >= min) & (data[column] <= max)]
 
     return data
+
+
+def removeTen(data,column,percent):
+    data =data.sort_values(by = column)
+    length = len(data)
+    # 작은거 자르기
+    cut = length // percent
+    data = data.iloc[cut:,:]
+
+    # 큰거 자르기
+    data =data.sort_values(by =column , ascending=False)
+    cut = length - 2 * cut
+    data = data.iloc[:cut,:]
+
+    return data
+
+def thirtyTwo(food):
+    food_count=food.pivot_table(index=['상권_코드', '상권_코드_명', '서비스_업종_코드', '서비스_업종_코드_명'],aggfunc='size')
+    food_count=pd.DataFrame(food_count[food_count == 32]).reset_index()
+    code=zip(food_count['상권_코드'],food_count['서비스_업종_코드'])
+    food_temp = food[food[['상권_코드','서비스_업종_코드']].apply(tuple,1).isin(code)]
+    
+    return food_temp
